966d48662cb327aa7beaaa27111f027156e87967
Halil İbrahim Durmuş <themachinepoi@gmail.com>
2020-08-27 23:42:53 +0300

    Remove old files

--- pharmacies/pharmacies-route.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-
-const router = express.Router();
-const pharmaciesController = require('../pharmacies/pharmacies-controller');
-
-// POST request for listing Pharmacies on Duty in specific District.
-router.post('/', pharmaciesController.getPharmaciesOnDuty);
-
-module.exports = router;
--- server.js
+++
@@ -1,6 +0,0 @@
-const config = require('config');
-
-const serverConfig = config.get('serverConfig');
-const app = require('./app');
-
-app.listen(serverConfig.port, serverConfig.host, () => console.log(`\nPharmacies on Duty API started running on: http://${serverConfig.host}:${serverConfig.port}${serverConfig.apiPrefix}`));
